f84182ea7509337aaaf7cf4f5985512209c9383f
gemlmc <gmluch@scl.edu.gt>
2025-08-12 13:17:23 -0600

    datos backE

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2');
 
 const pool = mysql2.createPool({
-    host: 'brktcoraxhoslbbecgah-mysql.services.clever-cloud.com',
-    user: 'u4fke0udkgvuukdu',
-    password: 'RVWL5YlgzAgOpUQVyLxB',
-    database: 'brktcoraxhoslbbecgah',
+    host: 'bv5o7ejzi4yfpe6nmmvt-mysql.services.clever-cloud.com',
+    user: 'u04yuoivggmh9sae',
+    password: 'u04yuoivggmh9sae',
+    database: 'bv5o7ejzi4yfpe6nmmvt',
 });
 
 module.exports = pool.promise();
