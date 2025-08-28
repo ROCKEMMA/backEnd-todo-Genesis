9ed22214d1e0cc0649f0afd3ac8fa1861ca35111
gemlmc <gmluch@scl.edu.gt>
2025-08-28 08:52:34 -0600

    se actualiza backend

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* const db = require('./config/database');
  */require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
 
-const app = express();   
+const app = express();  
 
+app.use(cors({
+    origin: "*",
+    methods: ['GET','POST','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+//RUTAS TIPO GET
 const getTablas = require('./routes/get/obtenerTablas');
 app.use(getTablas);
+
+const getTareas = require('./routes/get/obtenerTareas');
+app.use(getTareas);
+
+
 
 // MIS RUTAS MIDDLEWARE
 /* app.get('/',(req,res)=>{
---
+++ routes/get/obtenerTareas.js
@@ -0,0 +1,27 @@
+const db = require('../../config/database');
+const express = require('express');
+const router = express.Router();
+
+router.get('/tareas',async(req,res)=>{
+    try {
+        
+          let [todasLasTareas] = await db.query(`
+            SELECT
+                id,
+                nombre,
+                descripcion,
+                estado_tarea,
+                DATE_FORMAT(fecha_asignada, '%Y-%m-%d') AS fecha_asignada,
+                DATE_FORMAT(fecha_entrega, '%Y-%m-%d') AS fecha_entrega,
+                estado_artivado,
+                usuario_id
+            FROM tareas`);
+        res.json(todasLasTareas);
+
+    } catch (error) {
+        console.error("Error: ", error);
+        
+    }
+});
+
+module.exports = router;
