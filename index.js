f921c4f7779aed93cca3daa387ebf00517e19552
gemlmc <gmluch@scl.edu.gt>
2025-08-28 23:00:17 -0600

    Actualización

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();  
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5500'],
+    origin: "*",
     methods: ['GET','POST','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
