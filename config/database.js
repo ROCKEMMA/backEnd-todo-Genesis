0e7a8987a072ef9f8ae22734f4eec5985556221f
gemlmc <gmluch@scl.edu.gt>
2025-08-29 07:37:31 -0600

    Actualización

--- config/database.js
+++ config/database.js
@@ -1,23 +1,10 @@
-/* const mysql2 = require('mysql2');
+const mysql2 = require('mysql2');
 
 const pool = mysql2.createPool({
     host: 'b3kqozfueweuqm7uayjk-mysql.services.clever-cloud.com',
     user: 'unpy22jt4bwgpans',
     password: 'm0maetxYj4NcbokWOZqm',
     database: 'b3kqozfueweuqm7uayjk',
-});
-
-module.exports = pool.promise(); */
-
-const mysql2 = require('mysql2');
-
-const pool = mysql2.createPool({
-    host: "b3kqozfueweuqm7uayjk-mysql.services.clever-cloud.com",        // ej: "mysql-xxxx.render.com"
-    user: "unpy22jt4bwgpans",               
-    password: "m0maetxYj4NcbokWOZqm",         
-    database: "mysql -h b3kqozfueweuqm7uayjk-mysql.services.clever-cloud.com -P 3306 -u unpy22jt4bwgpans -p b3kqozfueweuqm7uayjk",     // el nombre de tu base de datos
-    port: 3306,                       
-    ssl: { rejectUnauthorized: true } 
 });
 
 module.exports = pool.promise();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+/* const db = require('./config/database');
+ */require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
@@ -6,24 +7,30 @@ const cors = require('cors');
 const app = express();  
 
 app.use(cors({
-    origin: "*",
+    origin: ['http://127.0.0.1:5500', ' https://marianaseven2013.github.io/todo_list/'],
     methods: ['GET','POST','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
-
+//RUTAS TIPO GET
 const getTablas = require('./routes/get/obtenerTablas');
 app.use(getTablas);
 
 const getTareas = require('./routes/get/obtenerTareas');
 app.use(getTareas);
 
-app.get('/', (req, res) => {
-    res.send("✅ Backend funcionando en Render - Usa /tareas para obtener datos");
+
+
+// MIS RUTAS MIDDLEWARE
+/* app.get('/',(req,res)=>{
+
+    let consulta = db.query('SHOW TABLES')
+    res.send("Mi backEnd con ExpressJS");
 });
+ */
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Servidor: http://localhost:${PORT}`);
+})
+
